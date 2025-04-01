8c60e443a962034dc696ee74fbd63819349451d0
cerysleung <cerysl2027@student.cis.edu.hk>
2025-04-01 08:48:07 +0800

    lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class Dateable {
 
   public static void main(String[] args) {
-      // Write your code below, inside the main method.
-    
+    // Write your code below, inside the main method.
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter your age: ");
+    int age = scanner.nextInt();
+    int youngestAge = 7 + (age / 2);
+    System.out.println(age + "-year olds should date someone who is at least " + youngestAge + " years old.");
   }
-  
 }
